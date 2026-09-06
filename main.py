2954d0b550d50609cdca8b0b40cd6e70b3975f0d
obviouslyweb <conbutter12@gmail.com>
2026-09-05 21:34:37 -0400

    Updated version number

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 import time
 
 # Bot version number
-VERSION = "2.8.0"
+VERSION = "2.8.1"
 
 # Print Discord.py version and file load location for debug
 print("Discord.py Version:", discord.__version__)
